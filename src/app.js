e98a35316c14f4dd371e917c995f2e343f736fe0
Anthony Ellis <aellisnola@gmail.com>
2019-12-16 10:03:36 -0600

    13.133 Finished .Added seperate build and dev scripts for webpack. Added conditional source-mapping

--- src/app.js
+++ src/app.js
@@ -1,36 +1,22 @@
-// THIRD-PARTY
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-
-// ROUTERS
 import AppRouter from './routers/AppRouter'
-
-// COMPONENTS
-
-// STORES
 import configureStore from './store/configureStore'
-
-// ACTIONS
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
-
-// SELECTORS
 import getVisibleExpenses from './selectors/expenses'
-
-// STYLES
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css'
 
-
 const store = configureStore();
-
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
+
 ReactDOM.render(jsx, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader', 
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    console.log('env', env)
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map': 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 }
