a87734f81d5367bcf51a8f51e584275160fc246f
Anthony Ellis <aellisnola@gmail.com>
2019-12-16 14:51:28 -0600

    13.134 complete. Seperated CSS files from the production build, segmenting the production app further and making it smaller. Added css to index.html. Tweaked for better css sourcemap functionality.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env) => {
     const isProduction = env === 'production';
 
     return {
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            })
+        ],
         entry: './src/app.js',
         output: {
             path: path.resolve(__dirname, 'public'),
@@ -17,13 +23,22 @@ module.exports = (env) => {
             }, {
                 test: /\.s?css$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        } 
+                    },{
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        } 
+                    },
                 ]
             }]
         },
-        devtool: isProduction ? 'source-map': 'cheap-module-eval-source-map',
+        devtool: isProduction ? 'source-map': 'inline-source-map',
         devServer: {
             contentBase: path.resolve(__dirname, 'public'),
             historyApiFallback: true
