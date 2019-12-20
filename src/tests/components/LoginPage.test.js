41b614cf914a6db841881e7568dd743e96afcc4a
Anthony Ellis <aellisnola@gmail.com>
2019-12-20 14:31:57 -0600

    16.163 done. Added log-out functionality and tests for startLogin/startLogout actions.

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { LoginPage } from '../../components/LoginPage'
+import { startLogout } from '../../actions/auth'
 
 test("Should properly render LoginPage component", () => {
     const wrapper = shallow(<LoginPage />)
     expect(wrapper).toMatchSnapshot()
 })
+
+test("Should call startLogin on button click", () => {
+    const startLogin = jest.fn()
+    const wrapper = shallow(<LoginPage startLogin={startLogin} />)
+    wrapper.find('button').simulate('click')
+    expect(startLogin).toHaveBeenCalled()
+})
+
