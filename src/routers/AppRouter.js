fd53b54dda88ee02e6cdfbcbabdcf19f00d94e15
Anthony Ellis <aellisnola@gmail.com>
2019-12-20 13:34:29 -0600

    16.162 complete. Rearranged app navigation. Added google pop-up authentication.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import Header from '../components/Header'
 import Dashboard from '../components/Dashboard'
 import AddExpense from '../components/AddExpense'
 import EditExpense from '../components/EditExpense'
 import Help from '../components/Help'
 import Error404 from '../components/Error404'
+import LoginPage from '../components/LoginPage'
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
+
             <Switch>
-                <Route path='/' component={Dashboard} exact={true} />
+                <Route path='/' component={LoginPage} exact={true} />
+                <Route path='/dashboard' component={Dashboard} />
                 <Route path='/add' component={AddExpense} />
                 <Route path='/edit/:id' component={EditExpense} />
                 <Route path='/help' component={Help} />
+
                 <Route component={Error404} />
             </Switch>
         </div>
