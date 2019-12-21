e36c0686d14c7c71682071d9ad870bf92cfe94f7
Anthony Ellis <aellisnola@gmail.com>
2019-12-21 12:30:14 -0600

    16.168 done. User data now in user area. Only accessible by proper user.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -5,13 +5,15 @@ import database from '../../firebase/firebase'
 import expenses from '../fixtures/expenses'
 
 const createMockStore = configureMockStore([thunk])
+const uid = 'abc123'
+const defaultAuthState = { auth: { uid } }
 
 beforeEach((done) => {
     const expensesData = {}
     expenses.forEach(({ id, description, note, amount, createdAt }) => {
         expensesData[id] = { description, note, amount, createdAt }
     })
-    database.ref('expenses').set(expensesData).then(() => done())
+    database.ref(`users/${uid}/expenses`).set(expensesData).then(() => done())
 })
 
 test('Should generate an action object to removeExpense', () => {
@@ -23,7 +25,7 @@ test('Should generate an action object to removeExpense', () => {
 })
 
 test("Should remove expense from firebase.", (done) => {
-    const store = createMockStore({})
+    const store = createMockStore(defaultAuthState)
     const id = expenses[2].id
 
     store.dispatch(startRemoveExpense({ id })).then(() => {
@@ -32,7 +34,7 @@ test("Should remove expense from firebase.", (done) => {
             type: 'REMOVE_EXPENSE',
             id
         })
-        return database.ref(`expenses/${id}`).once('value')
+        return database.ref(`users/${uid}/expenses/${id}`).once('value')
     }).then((snapshot) => {
         expect(snapshot.val()).toBeFalsy()
         done()
@@ -57,7 +59,7 @@ test('Should generate an action object to editExpense.', () => {
 })
 
 test("Should edit expense from firebase.", (done) => {
-    const store = createMockStore({})
+    const store = createMockStore(defaultAuthState)
     const id = expenses[0].id
     const updates = {
         amount: 12045
@@ -70,7 +72,7 @@ test("Should edit expense from firebase.", (done) => {
             id,
             updates
         })
-        return database.ref(`expenses/${id}`).once('value')
+        return database.ref(`users/${uid}/expenses/${id}`).once('value')
     }).then((snapshot) => {
         expect(snapshot.val().amount).toBe(12045)
         done()
@@ -86,7 +88,7 @@ test('Should generate an action object to addExpense with the given values', () 
 })
 
 test('Should add expense to database and store', (done) => {
-    const store = createMockStore({})
+    const store = createMockStore(defaultAuthState)
     const expenseData = {
         description: 'Mouse',
         amount: '3000',
@@ -105,7 +107,7 @@ test('Should add expense to database and store', (done) => {
             }
         })
 
-        return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+        return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once('value')
     }).then((snapshot) => {
         expect(snapshot.val()).toEqual(expenseData)
         done()
@@ -113,7 +115,7 @@ test('Should add expense to database and store', (done) => {
 })
 
 test('Should add expense with defaults to database and store', (done) => {
-    const store = createMockStore({})
+    const store = createMockStore(defaultAuthState)
     const defaultData = {
         description: '',
         note: '',
@@ -132,7 +134,7 @@ test('Should add expense with defaults to database and store', (done) => {
             }
         })
 
-        return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+        return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once('value')
     }).then((snapshot) => {
         expect(snapshot.val()).toEqual(defaultData)
         done()
@@ -148,7 +150,7 @@ test("Should set up setExpense action object with data.", () => {
 })
 
 test("Should fetch expenses from fireabse.", () => {
-    const store = createMockStore({})
+    const store = createMockStore(defaultAuthState)
     store.dispatch(startSetExpenses()).then((done) => {
         const actions = store.getActions()
         expect(actions[0]).toEqual({
